cdb489dec204ba3940b24feb51a1ec2fc89e6b68
Kamelia Ikonomova <61159710+KameliaIkonomova@users.noreply.github.com>
2024-10-23 15:31:51 +0300

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,18 +1,10 @@
-from django.urls import path
-
-from . import views 
+from django.contrib import admin
+from django.urls import path, include 
 
 
 urlpatterns = [
-	path("", views.Index, name = "index"),
-	# path to detail view  of a poll (here options will be shown and user can vote)
-	path("poll/detail/<int:pk>/", views.DetailPoll, name = 'detail-poll'),
-	# path to create a new option
-	path("create/option/<int:pk>/", views.CreateOption, name = 'create-option'),
-	# path to create a new poll
-	path("create/poll", views.CreatePoll, name = "create-poll"),
-	# path to create a new annnouncement
-	path("create/announcement", views.CreateAnnouncement, name = "create-announcement"),
-	# path to vote 
-	path("vote/poll/<int:pk>/", views.VotePoll, name = 'vote-poll'),
+    path('admin/', admin.site.urls),
+    # redirecting requests to the main and users app
+    path('', include("main.urls")),
+    path('users/', include('users.urls')),
 ]
